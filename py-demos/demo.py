b084576c612c21f942009e08dd38b91adea3a9db
sanaullah525 <sanasanaullah524@gmail.com>
2025-10-27 02:02:01 +0500

    Some basic arithmetics using python language.

--- py-demos/demo.py
+++ py-demos/demo.py
@@ -1,1 +1,8 @@
-print("Welcome to the python world !!!")
+print("Welcome to the python world !!!", 
+      "This is my first demo script.",
+    "Python is fun to learn and use.", "Happy coding!", 
+        "Let's explore more Python features.")
+print("This is to print some arithmetics" \
+" using python: 21+4= ", 21+4)
+print("Multiplication: 7*6= ", 7*6)
+print("Division: 56/8= ", 56/8)
